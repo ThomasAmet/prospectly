8036c6f66f4dad51aec1e2068184ea781b27ce15
Thomas Amet <thomas.amet@essec.edu>
2020-04-10 13:14:58 +0200

    bug generator

--- app/leads/views.py
+++ app/leads/views.py
@@ -37,7 +37,6 @@ from ..models import User, Subscription, Company, Contact, CompanyLead, ContactL
 # @leads.route('/prospectly-generator', methods=['POST', 'GET'])
 @login_required
 @pro_plan_required
-@valid_subscription_required
 def view_leads():
 	comp_lead_form = CompaniesQueryForm()
 	
